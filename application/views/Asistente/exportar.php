c9826227fe850db97f3daff31b70f3d1233cf559
Carlangas21 <carlos.rojasm96@gmail.com>
2017-07-06 02:53:35 -0400

    Excel funcando en ambos, menos ayudante

--- application/views/Asistente/exportar.php
+++ application/views/Asistente/exportar.php
@@ -6,26 +6,21 @@
 <div class="card">
 	                            <div class="card-header" data-background-color="purple">
 	                                <h4 class="title">Exportación de datos</h4>
-	                                <p class="category">Acá puedes exportar tus datos a excel </p>
+	                                <p class="category"><i class="fa fa-file-excel-o"></i> Puedes exportar tus datos de:</p>
 	                            </div>
 	                            <div class="card-content table-responsive">
 	                              <div id="links" class="section scrollspy">
            						 <div class="collection">
-           						   <a href="#modal-default" data-toggle="modal" data-target="#modal-default" class="collection-item">Alumnos</a>
-           						   <a href="#modal-default" data-toggle="modal" data-target="#modal-default" class="collection-item ">Profesores</a>
-           						   <a href="#modal-default" data-toggle="modal" data-target="#modal-default" class="collection-item">Tutorías</a>
-           						   <a href="#modal-default" data-toggle="modal" data-target="#modal-default" class="collection-item">Reforzamiento</a>
+           						   <a href="<?=site_url('Excel_Controller/downloadAlum')?>" class="collection-item"><i class="fa fa-user"> </i> Alumnos</a>
+           						   <a href="<?=site_url('Excel_Controller/downloadTutores')?>" class="collection-item "><i class="fa fa-user-secret"> </i> Tutores</a>
+           						   <a href="<?=site_url('Excel_Controller/downloadTutorias')?>"  class="collection-item"><i class="fa fa-check-circle"> </i> Tutorías Realizadas</a>
+           						   <a href="<?=site_url('Excel_Controller/downloadRefor')?>"  class="collection-item"><i class="fa fa-book"> </i> Reforzamiento</a>
            						 </div>
 	                            </div>
 	                        </div>
 	                    </div>
-	                   	    
-	                    	
-								  
-
 						<div class="col-lg-3 col-md-6 col-sm-6">
 							<div class="card card-stats">
-								
 							</div>
 						</div>
 						<div class="col-lg-3 col-md-6 col-sm-6">
@@ -39,30 +34,8 @@
 								
 								</div>
 							</div>
-						
+							</div>
+							</div>
+							</div>
 
-
-
-							</div></div></div>
-
-<div class="modal fade" id="modal-default">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">¿Seguro que desea descargar el archivo?</h4>
-              </div>
-              <div class="modal-body">
-                <p>One fine body&hellip;</p>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Cancelar</button>
-                <button type="button" class="btn btn-primary">Aceptar</button>
-              </div>
-            </div>
-            <!-- /.modal-content -->
-          </div>
-          <!-- /.modal-dialog -->
-        </div>
         <!-- /.modal -->
--- application/views/Asistente/importar.php
+++ application/views/Asistente/importar.php
@@ -6,22 +6,22 @@
 <div class="card">
 	                            <div class="card-header" data-background-color="purple">
 	                                <h4 class="title">Importación de datos</h4>
-	                                <p class="category">Acá puedes importar tus datos </p>
+	                                <p class="category"><i class="fa fa-file-excel-o"></i> Puedes importar los datos de:</p>
 	                            </div>
 	                            <div class="card-content table-responsive">
 	                              <div id="links" class="section scrollspy">
            						 <div class="collection">
-           						   <a href="#myModal" data-toggle="modal" class="collection-item">Alumnos</a>
-           						   <a href="#myModal" data-toggle="modal" class="collection-item ">Profesores</a>
-           						   <a href="#myModal" data-toggle="modal" class="collection-item">Tutorías</a>
-           						   <a href="#myModal" data-toggle="modal" class="collection-item">Reforzamiento</a>
+           						   <a href="#myModal" data-toggle="modal" class="collection-item"><i class="fa fa-user"> </i>  Alumnos </a>
+           						   <a href="#myModal2" data-toggle="modal" class="collection-item"><i class="fa fa-user"> </i> Alumnos Ayudantes</a>
+           						   <a href="#myModal4" data-toggle="modal" class="collection-item "><i class="fa fa-user-secret"> </i> Tutores</a>
+           						   <a href="#myModal5" data-toggle="modal" class="collection-item "><i class="fa fa-user-secret"> </i> Tutores Progresión</a>
            						 </div>
 	                            </div>
 	                        </div>
 	                    </div>
 	                   	   
 	                    	
-								  <!-- MODAL IMPORTACIÓN COMÚN -->
+						 <!-- MODAL IMPORTACIÓN COMÚN -->
 						<div class="col-lg-3 col-md-6 col-sm-6">
 								<div class="modal fade" id="myModal">
 								<div class="modal-dialog">
@@ -32,21 +32,127 @@
 								        </div>
 								        <div class="modal-body" style="text-align: center;">
 										  <form action="<?=site_url('Excel_Controller/UsuarioUploader')?>" method="post" enctype="multipart/form-data">
-        <input type="file"  name="file" style="margin-left: 25%"> 
-        <button type="submit" class="btn btn-primary" style="margin-top: 5%;">Importar</button>
-    </form>
+										        <input type="file" accept=".csv" name="file" required="required" style="margin-left: 25%"> 
+										        <button type="submit" class="btn btn-success"  style="margin-top: 5%;"><i class="fa fa-upload"> </i> Importar</button>
+										    </form>
 										  
 										</div>
 								        <div class="modal-footer">
-								          <button type="button" class="btn btn-default " data-dismiss="modal">Cancelar</button>
+								          <button type="button" class="btn btn-danger " data-dismiss="modal"><i class="fa fa-remove"> </i> Cancelar</button>
 								        </div>
-												
-
 								      </div><!-- /.modal-content -->
 								    </div><!-- /.modal-dialog -->
 								  </div><!-- /.modal -->
-						</div></div>
+						</div>
+						</div>
 						<!-- FIN MODAL IMPORTACIÓN COMÚN -->
+
+
+						 <!-- MODAL IMPORTACIÓN COMÚN -->
+						<div class="col-lg-3 col-md-6 col-sm-6">
+								<div class="modal fade" id="myModal2">
+								<div class="modal-dialog">
+								      <div class="modal-content">
+								        <div class="modal-header">
+								          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+								      
+								        </div>
+								        <div class="modal-body" style="text-align: center;">
+										  <form action="<?=site_url('Excel_Controller/UsuarioUploader')?>" method="post" enctype="multipart/form-data">
+										        <input type="file" accept=".csv" name="file" required="required" style="margin-left: 25%"> 
+										        <button type="submit" class="btn btn-success"  style="margin-top: 5%;"><i class="fa fa-upload"> </i> Importar</button>
+										    </form>
+										  
+										</div>
+								        <div class="modal-footer">
+								          <button type="button" class="btn btn-danger " data-dismiss="modal"><i class="fa fa-remove"> </i> Cancelar</button>
+								        </div>
+								      </div><!-- /.modal-content -->
+								    </div><!-- /.modal-dialog -->
+								  </div><!-- /.modal -->
+						</div>
+						</div>
+						<!-- FIN MODAL IMPORTACIÓN COMÚN -->
+
+ 					<!-- MODAL IMPORTACIÓN COMÚN -->
+						<div class="col-lg-3 col-md-6 col-sm-6">
+								<div class="modal fade" id="myModal3">
+								<div class="modal-dialog">
+								      <div class="modal-content">
+								        <div class="modal-header">
+								          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+								      
+								        </div>
+								        <div class="modal-body" style="text-align: center;">
+										  <form action="<?=site_url('Excel_Controller/UsuarioUploader')?>" method="post" enctype="multipart/form-data">
+										        <input type="file" accept=".csv" name="file" required="required" style="margin-left: 25%"> 
+										        <button type="submit" class="btn btn-success"  style="margin-top: 5%;"><i class="fa fa-upload"> </i> Importar</button>
+										    </form>
+										  
+										</div>
+								        <div class="modal-footer">
+								          <button type="button" class="btn btn-danger " data-dismiss="modal"><i class="fa fa-remove"> </i> Cancelar</button>
+								        </div>
+								      </div><!-- /.modal-content -->
+								    </div><!-- /.modal-dialog -->
+								  </div><!-- /.modal -->
+						</div>
+						</div>
+						<!-- FIN MODAL IMPORTACIÓN COMÚN -->
+
+ <!-- MODAL IMPORTACIÓN COMÚN -->
+						<div class="col-lg-3 col-md-6 col-sm-6">
+								<div class="modal fade" id="myModal4">
+								<div class="modal-dialog">
+								      <div class="modal-content">
+								        <div class="modal-header">
+								          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+								      
+								        </div>
+								        <div class="modal-body" style="text-align: center;">
+										  <form action="<?=site_url('Excel_Controller/UsuarioTutoria')?>" method="post" enctype="multipart/form-data">
+										        <input type="file" accept=".csv" name="file" required="required" style="margin-left: 25%"> 
+										        <button type="submit" class="btn btn-success"  style="margin-top: 5%;"><i class="fa fa-upload"> </i> Importar</button>
+										    </form>
+										  
+										</div>
+								        <div class="modal-footer">
+								          <button type="button" class="btn btn-danger " data-dismiss="modal"><i class="fa fa-remove"> </i> Cancelar</button>
+								        </div>
+								      </div><!-- /.modal-content -->
+								    </div><!-- /.modal-dialog -->
+								  </div><!-- /.modal -->
+						</div>
+						</div>
+						<!-- FIN MODAL IMPORTACIÓN COMÚN -->
+
+ 					<!-- MODAL IMPORTACIÓN COMÚN -->
+						<div class="col-lg-3 col-md-6 col-sm-6">
+								<div class="modal fade" id="myModal5">
+								<div class="modal-dialog">
+								      <div class="modal-content">
+								        <div class="modal-header">
+								          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+								      
+								        </div>
+								        <div class="modal-body" style="text-align: center;">
+										  <form action="<?=site_url('Excel_Controller/UsuarioTutoriaPermanente')?>" method="post" enctype="multipart/form-data">
+										        <input type="file" accept=".csv" name="file" required="required" style="margin-left: 25%"> 
+										        <button type="submit" class="btn btn-success"  style="margin-top: 5%;"><i class="fa fa-upload"> </i> Importar</button>
+										    </form>
+										  
+										</div>
+								        <div class="modal-footer">
+								          <button type="button" class="btn btn-danger " data-dismiss="modal"><i class="fa fa-remove"> </i> Cancelar</button>
+								        </div>
+								      </div><!-- /.modal-content -->
+								    </div><!-- /.modal-dialog -->
+								  </div><!-- /.modal -->
+						</div>
+						</div>
+						<!-- FIN MODAL IMPORTACIÓN COMÚN -->
+
+
 
 						<div class="col-lg-3 col-md-6 col-sm-6">
 							<div class="card card-stats">
@@ -64,8 +170,6 @@
 								
 								</div>
 							</div>
-						
-
-
-
-							</div></div></div>
+						</div>
+					</div>
+				</div>
